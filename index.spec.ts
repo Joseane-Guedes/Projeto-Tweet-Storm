50f12e6191083993ee7a02f54c0ac44c37bd402e
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-07-10 22:20:49 -0300

    Refactor: Improve code structure

--- index.spec.ts
+++ index.spec.ts
@@ -1,56 +1,30 @@
 import { createTweetStorm } from './index';
 
 describe('createTweetStorm', () => {
-    it('should split the text into tweets with correct pagination', () => {
-        const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod consequat arcu, non interdum massa consequat id.';
+    it('should split a long text into multiple tweets', () => {
+        const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis orci sit amet magna lacinia, sed congue libero ultrices.';
         const expectedTweets = [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod consequat arcu, non interdum massa consequat id. 1/1"
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis orci sit amet magna lacinia, sed congue libero ultrices. [1/1]"
         ];
-
         const tweets = createTweetStorm(text);
-
         expect(tweets).toEqual(expectedTweets);
     });
 
-    it('should handle text shorter than maxLength', () => {
-        const text = 'Short text.';
-        const expectedTweets = ["Short text. 1/1"];
-
+    it('should split a text shorter than the maximum length into a single tweet', () => {
+        const text = 'Short text that fits in a single tweet.';
+        const expectedTweets = ['Short text that fits in a single tweet. [1/1]'];
         const tweets = createTweetStorm(text);
-
         expect(tweets).toEqual(expectedTweets);
     });
 
     it('should handle empty text', () => {
         const text = '';
-        const expectedTweets = [];
-
+        const expectedTweets: string[] = [];
         const tweets = createTweetStorm(text);
-
         expect(tweets).toEqual(expectedTweets);
     });
-
-    it('should calculate the correct pagination', () => {
-        const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
-        const expectedTweets = [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 1/1',
-        ];
-
-        const tweets = createTweetStorm(text);
-
-        expect(tweets).toEqual(expectedTweets);
-    });
-
-    it('should handle text that fits in a single tweet', () => {
-        const text = 'Short text that fits in a single tweet.';
-        const expectedTweets = ['Short text that fits in a single tweet. 1/1'];
-
-        const tweets = createTweetStorm(text);
-
-        expect(tweets).toEqual(expectedTweets);
-    });
-
 });
+
 
 
 
--- index.ts
+++ index.ts
@@ -1,25 +1,18 @@
 export function createTweetStorm(text: string): string[] {
-    const maxLength = 140 - 4; // Considerando o espaço para [PAG]/[TOT]
+    const maxLength = 136; // Considerando o espaço para [PAG]/[TOT]
     const tweets: string[] = [];
 
-    // Loop para dividir o texto em tweets
-    while (text.length > maxLength) {
-        const tweet = text.substring(0, maxLength); // Extrai o tweet do início do texto
-        text = text.substring(maxLength); // Remove o tweet do texto original
-
-        // Cálculo da paginação do tweet
-        const pagination = `${tweets.length + 1}/${Math.ceil(text.length / maxLength) + 1}`;
-        tweets.push(`${tweet} ${pagination}`); // Adiciona o tweet com a paginação ao array de tweets
+    for (let i = 0; i < text.length; i += maxLength) {
+        const tweet = text.substring(i, i + maxLength);
+        tweets.push(tweet);
     }
 
-    // Último tweet, caso o texto não caiba completamente em um tweet
-    if (text.length > 0) {
-        const pagination = `${tweets.length + 1}/${tweets.length + 1}`; // Páginação para o último tweet
-        const tweetWithPagination = `${text} ${pagination}`; // Último tweet com a páginação
-        tweets.push(tweetWithPagination); // Adiciona o último tweet ao array de tweets
-    }
+    const totalPages = tweets.length;
+    const numberedTweets = tweets.map((tweet, index) => `${tweet} [${index + 1}/${totalPages}]`);
 
-    return tweets; // Retorna o array de tweets
+    return numberedTweets;
 }
+
+
 
 
